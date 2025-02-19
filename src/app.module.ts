ba6742add6007e342e84fb0488365ec3ec9810c3
alex-claros <alex.claros@assuresoft.com.bo>
2025-02-19 12:35:29 -0400

    modulo, controlador, servicio usuarios configuracion inicial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "123456789",
+      database: "users",
+      entities: [__dirname + "/**/*.entity{.ts,.js}"]
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
