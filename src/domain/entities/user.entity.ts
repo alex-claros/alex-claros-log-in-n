7d9ddeea2e26b2f7f0a28a4dc2d21a5f2e478721
alex.claros <alex.claros@assuresoft.com.bo>
2025-02-24 16:37:05 +0000

    actualizacion entidad users

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,13 +1,23 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn } from 'typeorm';
+import { Task } from './task.entity';
 
-@Entity('users')
+@Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  name: string;
 
   @Column({ unique: true })
   email: string;
 
   @Column()
   password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @OneToMany(() => Task, task => task.user)
+  tasks: Task[];
 }
